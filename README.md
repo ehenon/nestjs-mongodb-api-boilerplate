b1c2cc962284a75849605538b74e8ec8adf35261
ehenon <emilienhenon@gmail.com>
2022-12-15 22:58:33 +0100

    feat: initialize repository ✨

---
+++ README.md
@@ -0,0 +1,34 @@
+# NestJS + MongoDB API boilerplate
+
+REST API template using NestJS and MongoDB, with:
+- lint
+- unit tests
+- CI workflow
+- custom JSON logger
+- `.env` configuration system
+- OpenAPI spec
+- Conventional commits spec
+
+## Available commands
+
+```
+npm run start:dev
+npm run start:debug
+npm run lint
+npm run test
+npm run build
+npm run start
+npm run start:prod
+```
+
+## Tools
+
+- [NestJS](https://nestjs.com/) framework 🐱
+- [Express](https://expressjs.com/fr/) http server framework 🚀
+- [Mongoose](https://mongoosejs.com/) ODM 🍃
+- [ESLint](https://eslint.org/) linter 💎
+- [Airbnb](https://www.npmjs.com/package/eslint-config-airbnb-typescript) style guide 🎨
+- [Jest](https://jestjs.io/) testing framework 🧪
+- [Husky](https://typicode.github.io/husky/#/) hooks 🐶
+- [Conventional commits](https://www.conventionalcommits.org/en/v1.0.0/) specification 📚
+- [GitHub Actions](https://docs.github.com/en/actions) workflow ⚙️
---
+++ src/instruments/instruments.service.ts
@@ -0,0 +1,54 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { v1 as uuidv1 } from 'uuid';
+import random from 'lodash.random';
+import { CreateInstrumentDTO } from './dto/create-instrument.dto';
+import { UpdateInstrumentDTO } from './dto/update-instrument.dto';
+import { InstrumentDocument } from './schemas/instrument.schema';
+
+@Injectable()
+export class InstrumentsService {
+  constructor(
+    @InjectModel('Instrument') private readonly InstrumentModel: Model<InstrumentDocument>,
+  ) { }
+
+  async create(createInstrumentDTO: CreateInstrumentDTO): Promise<InstrumentDocument> {
+    const newInstrument = new this.InstrumentModel(createInstrumentDTO);
+    newInstrument.identifier = uuidv1();
+    return newInstrument.save();
+  }
+
+  async findAll(): Promise<InstrumentDocument[]> {
+    return this.InstrumentModel.find().exec();
+  }
+
+  async findRandomDocuments(limit = 10): Promise<InstrumentDocument[]> {
+    const count = await this.InstrumentModel.countDocuments();
+    const skip = random(0, count - limit);
+    return this.InstrumentModel.find().skip(skip).limit(limit).exec();
+  }
+
+  async findOne(identifier: string): Promise<InstrumentDocument> {
+    const instrument = await this.InstrumentModel.findOne({ identifier }).exec();
+    if (!instrument) {
+      throw new NotFoundException();
+    }
+    return instrument;
+  }
+
+  async update(
+    identifier: string,
+    updateInstrumentDTO: UpdateInstrumentDTO,
+  ): Promise<InstrumentDocument> {
+    return this.InstrumentModel.findOneAndUpdate(
+      { identifier },
+      updateInstrumentDTO,
+      { returnOriginal: false },
+    ).exec();
+  }
+
+  async remove(identifier: string): Promise<InstrumentDocument> {
+    return this.InstrumentModel.findOneAndRemove({ identifier }).exec();
+  }
+}
